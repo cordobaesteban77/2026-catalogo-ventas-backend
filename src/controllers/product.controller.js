6fa6be2cc391d205aa7a01ddfa15adbaf41c7b61
Esteban <cordobaesteban77@gmail.com>
2026-08-13 20:35:34 -0300

    agrego el uso de try catch en los controladores de createProduct, updateProduct, disableProduct y deleteProduct

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -6,44 +6,60 @@ const getProducts = async (req, res) => {
         const products = await Product.find();
         res.status(200).json({ ok: true, products });
     } catch (error) {
+        console.error(error);
         return res.status(500),json({ ok: false, error: error.message });
     }
 };
 
 // crear producto
 const createProduct = async (req, res) => {
-    const { price, description, img } = req.body;
-    const name = req.body.name.toUpperCase();
-    // validacion si existe un producto con ese nombre
-    const productDB = await Product.findOne({ name });
-    if (productDB) {
-        return res.status(400).json({ ok: true, message: `El producto con el nombre ${productDB.name} ya existe` });
+    try {
+        const { price, description, img } = req.body;
+        const name = req.body.name.toUpperCase();
+        // validacion si existe un producto con ese nombre
+        const productDB = await Product.findOne({ name });
+        if (productDB) {
+            return res.status(400).json({ ok: true, message: `El producto con el nombre ${productDB.name} ya existe` });
+        }
+        const data = { name, price, description };
+        const product = new Product(data);
+        await product.save();
+        res.status(201).json({ ok:true, message: `El producto ${data.name} se guardó correctamente` })
+    } catch (error) {
+        console.error(error);
+        return res.status(500),json({ ok: false, error: error.message });
     }
-    const data = { name, price, description };
-    const product = new Product(data);
-    await product.save();
-    res.status(201).json({ ok:true, message: `El producto ${data.name} se guardó correctamente` })
 };
 
 // actualizar producto
 const updateProduct = async (req, res) => {
-    const { id } = req.params;
-    const { price, description, stock } = req.body;
-    // cuerpo que le mando a mongoose para que actualice la DB
-    let data = { price, description, stock };
-    if (req.body.name) {
-        // le agrego al objeto data la propiedad name
-        data.name = req.body.name.toUpperCase();
+    try {
+        const { id } = req.params;
+        const { price, description, stock } = req.body;
+        // cuerpo que le mando a mongoose para que actualice la DB
+        let data = { price, description, stock };
+        if (req.body.name) {
+            // le agrego al objeto data la propiedad name
+            data.name = req.body.name.toUpperCase();
+        }
+        await Product.findByIdAndUpdate(id, data);
+        res.status(200).json({ ok: true, message: "Producto actualizado con éxito" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500),json({ ok: false, error: error.message });
     }
-    await Product.findByIdAndUpdate(id, data);
-    res.status(200).json({ ok: true, message: "Producto actualizado con éxito" });
 };
 
 // desactivar producto
 const disableProduct = async (req, res) => {
-    const { id } = req.params;
-    await Product.findByIdAndUpdate(id, { state: false });
-    res.status(200).json({ ok: true, message: "Producto desactivado" });
+    try {
+        const { id } = req.params;
+        await Product.findByIdAndUpdate(id, { state: false });
+        res.status(200).json({ ok: true, message: "Producto desactivado" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500),json({ ok: false, error: error.message });
+    }
 };
 
 // eliminar producto
@@ -53,6 +69,7 @@ const deleteProduct = async (req, res) => {
         await Product.findByIdAndDelete(id);
         res.status(200).json({ ok: true, message: "Producto eliminado con éxito" });
     } catch (error) {
+        console.error(error);
         return res.status(500),json({ ok: false, error: error.message });
     }
 };
