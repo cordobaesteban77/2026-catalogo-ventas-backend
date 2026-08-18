fe6459419636994241cfc21e41c4f6cbc3e32111
Esteban <cordobaesteban77@gmail.com>
2026-08-17 22:49:43 -0300

    creo el controlador para actualizar una categoria

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -24,8 +24,28 @@ const createCategory = async (req, res) => {
         res.status(201).json({ ok: true, message: `La categoria ${name} se creó con éxito` });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ ok: false, error: error.message })
+        return res.status(500).json({ ok: false, error: error.message });
     }
 };
 
-export { getCategory, createCategory };
+// actualizar categoria
+const updateCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        // validar que no exista una categoria con el mismo nombre
+        const validateName = await Category.findOne({ name: name.toUpperCase() });
+        if (validateName) {
+            return res.status(400).json({ ok: false, message: "Ya existe una categoria con ese nombre" });
+        }
+        // validar que el id exista en la base de datos
+        const data = { name: name.toUpperCase() };
+        const category = await Category.findByIdAndUpdate(id, data, { new: true });
+        res.status(200).json({ ok: true, message: "Categoria actualizada", category });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ ok: false, error: error.message });
+    }
+};
+
+export { getCategory, createCategory, updateCategory };
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createCategory, getCategory } from "../controllers/category.controller.js";
+import { createCategory, getCategory, updateCategory } from "../controllers/category.controller.js";
 
 const router = Router();
 
 router.get("/", getCategory);
 router.post("/create", createCategory);
+router.put("/update:id", updateCategory);
 
 export default router;
