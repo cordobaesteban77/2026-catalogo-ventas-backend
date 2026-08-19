82a9f2dc9cf7811ba6157cf42c9ea7a5ef453771
Esteban <cordobaesteban77@gmail.com>
2026-08-19 17:09:09 -0300

    creo la funcion dentro del archivo del modelo del usuario para hashear la contraseña

--- src/models/User.js
+++ src/models/User.js
@@ -36,6 +36,15 @@ const UserSchema = new mongoose.Schema({
 },
 { timestamps: true });
 
+// hash password
+UserSchema.pre('save', async function(){
+    if (!this.isModified("password")) {
+        return
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", UserSchema);
 
 export default User;
